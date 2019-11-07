b37dc8bedcb8c29ce302280a9209c0ad168e4db7
cflb <cflb.educator@gmail.com>
2019-11-07 14:10:04 -0300

    add form e funcao para inserir dados do form no banco

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+from formulario import MensagemForm
+
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///minhasmensagens.db'
+app.config['SECRET_KEY'] = 'lsrhvanlvlqtaçcojgq'
 
 class Mensagem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,17 +15,24 @@ class Mensagem(db.Model):
     def __repre__(self):
         return self.mensagem
 
-
-
 @app.route('/')
 def index():
-    return render_template('index.html')
 
-@app.route('/nova-mensagem')
+    msgs = Mensagem.query.all()
+
+
+    return render_template('index.html', msgs=msgs)
+
+@app.route('/nova-mensagem', methods=['POST', 'GET'])
 def novaMensagem():
-    return render_template('mensagens.html')
+    form = MensagemForm()
+    if form.validate_on_submit():
+        msg = Mensagem()
+        msg.mensagem = form.mensagem.data
+        db.session.add(msg)
+        db.session.commit()
 
+    return render_template('mensagens.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
