ec6267580ea6bc8e597369b16991750bc4edcfb5
cflb <cflb.educator@gmail.com>
2019-11-21 08:05:12 -0300

    adcionando CadastroUsuarioForm

--- formulario.py
+++ formulario.py
@@ -1,7 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email
+
+class CadastroUsuarioForm(FlaskForm):
+    login = StringField('Login', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    botao = SubmitField('Cadastrar')
 
 class MensagemForm(FlaskForm):
+    titulo = StringField('Titulo', validators=[DataRequired()])
     mensagem = StringField('Mensagem', validators=[DataRequired()])
     botao = SubmitField('Enviar MSG')
